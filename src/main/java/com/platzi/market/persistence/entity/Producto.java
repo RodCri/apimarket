98c7eaab481ab531474b3a59e6dbe95b2e242192
RodCri <cristian.rodriguez1317@gmail.com>
2024-06-22 17:15:22 -0500

    add controller

--- src/main/java/com/platzi/market/persistence/entity/Producto.java
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -89,4 +89,12 @@ public class Producto {
     this.estado = estado;
   }
 
+  public Categoria getCategoria() {
+    return categoria;
+  }
+
+  public void setCategoria(Categoria categoria) {
+    this.categoria = categoria;
+  }
+
 }
